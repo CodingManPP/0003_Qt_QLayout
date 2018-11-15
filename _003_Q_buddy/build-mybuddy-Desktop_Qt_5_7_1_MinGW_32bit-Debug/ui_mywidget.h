ed965325db54cafcba2eea6723b44c61daea24c7
wsxingjun <1185889308@qq.com>
2018-11-15 10:21:11 +0800

    _003_Q_buddy/ 中英文标签设置伙伴关系

---
+++ _003_Q_buddy/build-mybuddy-Desktop_Qt_5_7_1_MinGW_32bit-Debug/ui_mywidget.h
@@ -0,0 +1,154 @@
+/********************************************************************************
+** Form generated from reading UI file 'mywidget.ui'
+**
+** Created by: Qt User Interface Compiler version 5.7.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MYWIDGET_H
+#define UI_MYWIDGET_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpinBox>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MyWidget
+{
+public:
+    QWidget *widget;
+    QVBoxLayout *verticalLayout;
+    QHBoxLayout *horizontalLayout_4;
+    QLabel *label;
+    QPushButton *pushButton;
+    QHBoxLayout *horizontalLayout_3;
+    QLabel *label_2;
+    QCheckBox *checkBox;
+    QHBoxLayout *horizontalLayout_2;
+    QLabel *label_3;
+    QLineEdit *lineEdit;
+    QHBoxLayout *horizontalLayout;
+    QLabel *label_4;
+    QSpinBox *spinBox;
+
+    void setupUi(QWidget *MyWidget)
+    {
+        if (MyWidget->objectName().isEmpty())
+            MyWidget->setObjectName(QStringLiteral("MyWidget"));
+        MyWidget->resize(256, 113);
+        widget = new QWidget(MyWidget);
+        widget->setObjectName(QStringLiteral("widget"));
+        widget->setGeometry(QRect(0, 0, 251, 107));
+        verticalLayout = new QVBoxLayout(widget);
+        verticalLayout->setSpacing(6);
+        verticalLayout->setContentsMargins(11, 11, 11, 11);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        horizontalLayout_4 = new QHBoxLayout();
+        horizontalLayout_4->setSpacing(6);
+        horizontalLayout_4->setObjectName(QStringLiteral("horizontalLayout_4"));
+        label = new QLabel(widget);
+        label->setObjectName(QStringLiteral("label"));
+
+        horizontalLayout_4->addWidget(label);
+
+        pushButton = new QPushButton(widget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+
+        horizontalLayout_4->addWidget(pushButton);
+
+
+        verticalLayout->addLayout(horizontalLayout_4);
+
+        horizontalLayout_3 = new QHBoxLayout();
+        horizontalLayout_3->setSpacing(6);
+        horizontalLayout_3->setObjectName(QStringLiteral("horizontalLayout_3"));
+        label_2 = new QLabel(widget);
+        label_2->setObjectName(QStringLiteral("label_2"));
+
+        horizontalLayout_3->addWidget(label_2);
+
+        checkBox = new QCheckBox(widget);
+        checkBox->setObjectName(QStringLiteral("checkBox"));
+
+        horizontalLayout_3->addWidget(checkBox);
+
+
+        verticalLayout->addLayout(horizontalLayout_3);
+
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2->setSpacing(6);
+        horizontalLayout_2->setObjectName(QStringLiteral("horizontalLayout_2"));
+        label_3 = new QLabel(widget);
+        label_3->setObjectName(QStringLiteral("label_3"));
+
+        horizontalLayout_2->addWidget(label_3);
+
+        lineEdit = new QLineEdit(widget);
+        lineEdit->setObjectName(QStringLiteral("lineEdit"));
+
+        horizontalLayout_2->addWidget(lineEdit);
+
+
+        verticalLayout->addLayout(horizontalLayout_2);
+
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        label_4 = new QLabel(widget);
+        label_4->setObjectName(QStringLiteral("label_4"));
+
+        horizontalLayout->addWidget(label_4);
+
+        spinBox = new QSpinBox(widget);
+        spinBox->setObjectName(QStringLiteral("spinBox"));
+
+        horizontalLayout->addWidget(spinBox);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+#ifndef QT_NO_SHORTCUT
+        label->setBuddy(pushButton);
+        label_2->setBuddy(checkBox);
+        label_3->setBuddy(lineEdit);
+        label_4->setBuddy(spinBox);
+#endif // QT_NO_SHORTCUT
+
+        retranslateUi(MyWidget);
+
+        QMetaObject::connectSlotsByName(MyWidget);
+    } // setupUi
+
+    void retranslateUi(QWidget *MyWidget)
+    {
+        MyWidget->setWindowTitle(QApplication::translate("MyWidget", "MyWidget", Q_NULLPTR));
+        label->setText(QApplication::translate("MyWidget", "&Button:", Q_NULLPTR));
+        pushButton->setText(QApplication::translate("MyWidget", "PushButton", Q_NULLPTR));
+        label_2->setText(QApplication::translate("MyWidget", "C&heckBox:", Q_NULLPTR));
+        checkBox->setText(QApplication::translate("MyWidget", "CheckBox", Q_NULLPTR));
+        label_3->setText(QApplication::translate("MyWidget", "\350\241\214\347\274\226\350\276\221\345\231\250(&L)", Q_NULLPTR));
+        label_4->setText(QApplication::translate("MyWidget", "\346\225\260\345\255\227\351\200\211\346\213\251\346\241\206(&N):", Q_NULLPTR));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MyWidget: public Ui_MyWidget {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MYWIDGET_H
